a244a59a6d32456d29f623bef393e065b7df3813
Fetiamohammed <fetia-awol@hotmail.com>
2023-12-20 12:22:47 +0100

    acafter implimenting the test on zipValidator

--- zipValidator.ts
+++ zipValidator.ts
@@ -1,3 +1,22 @@
+// export function validateZip(zip: string): boolean {
+//     return true; //  the first test with out adding actual implementation.
+//   }
+
 export function validateZip(zip: string): boolean {
-    return true; //  the first test with out adding actual implementation.
+    if (zip.length !== 5) {
+      return false;
+    }
+
+    for (const char of zip) {
+      if (!isNumeric(char)) {
+        return false;
+      }
+    }
+
+    return true;
   }
+
+  function isNumeric(char: string): boolean {
+    return '0' <= char && char <= '9';
+  }
+// after adding actual logic.
