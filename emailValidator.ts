f4a666837f4abcaafce54cefd6c98da2c6d6d593
Fetiamohammed <fetia-awol@hotmail.com>
2023-12-20 12:08:59 +0100

    emailValidation with actual test

--- emailValidator.ts
+++ emailValidator.ts
@@ -1,5 +1,11 @@
-// Testing before adding email validation logic
+// // Testing before adding email validation logic
+// export function validateEmail(email: string): boolean {
+//     return true;
+//   }
+ //   and with this the jest test returned true with ze first email but not the other
 export function validateEmail(email: string): boolean {
-    return true;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
   }
-//   and with this the jest test returned true with ze first email but not the other
+// after adding the basic validation logic all three passed with jest test.
