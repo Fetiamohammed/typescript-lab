d5461064912a21eea0e115f01ad83850cbad64b0
Fetiamohammed <fetia-awol@hotmail.com>
2023-12-20 12:47:18 +0100

    all test passed after the actual logic

--- htmlGenerator.ts
+++ htmlGenerator.ts
@@ -1,4 +1,11 @@
+// export function makeHeading(text: string, level: number): string {
+//     return `<h${level}>${text}</h${level}>`;
+//   }
+// // before adding the logic
 export function makeHeading(text: string, level: number): string {
+    if (level < 1 || level > 6) {
+      throw new Error('Heading level must be between 1 and 6.');
+    }
+
     return `<h${level}>${text}</h${level}>`;
   }
-// before adding the logic
