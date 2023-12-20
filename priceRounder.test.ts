88cef62edda3c1a4dd5c8700ccbc3363ee074c0c
Fetiamohammed <fetia-awol@hotmail.com>
2023-12-21 00:39:51 +0100

    test efter pattern parameter

--- priceRounder.test.ts
+++ priceRounder.test.ts
@@ -16,8 +16,14 @@ test('roundPrice rounds up properly for values with many decimal places', () => 
   expect(roundPrice(99.9999999)).toBe('100.00 SEK');
 });
 
-// added test cases for extended functionality
-test('roundPrice supports different currencies', () => {
-    expect(roundPrice(232.10542, 'NOK')).toBe('232.11 NOK');
-    expect(roundPrice(14, 'USD')).toBe('14.00 USD');
-    expect(roundPrice(1024.2048, 'EUR')).toBe('1024.21 EUR');})
+test('roundPrice supports different currencies with custom pattern', () => {
+  expect(roundPrice(232.10542, '%PRICE% NOK')).toBe('232.11 NOK');
+  expect(roundPrice(14, '$%PRICE%')).toBe('$14.00');
+  expect(roundPrice(1024.2048, 'USD %PRICE%')).toBe('USD 1024.21');
+});
+
+test('roundPrice supports different currencies with custom pattern and currency', () => {
+    expect(roundPrice(232.10542, '%PRICE% USD', 'EUR')).toBe('232.11 USD');
+    expect(roundPrice(14, '%PRICE% GBP', 'JPY')).toBe('14.00 GBP');
+    expect(roundPrice(1024.2048, 'CAD %PRICE%', 'AUD')).toBe('CAD 1024.21');
+      })
--- priceRounder.ts
+++ priceRounder.ts
@@ -8,10 +8,17 @@
 //     const formattedPrice = roundedPrice.toFixed(2);
 //     return `${formattedPrice} SEK`;
 //   }
-// but with code after adding logic it passed all
+// but with this code after adding logic it passed all
 
-export function roundPrice(price: number, currency: string = 'SEK'): string {
-    const roundedPrice = Math.ceil(price * 100) / 100;
-    return `${roundedPrice.toFixed(2)} ${currency}`;
-  }
+// export function roundPrice(price: number, currency: string = 'SEK'): string {
+//     const roundedPrice = Math.ceil(price * 100) / 100;
+//     return `${roundedPrice.toFixed(2)} ${currency}`;
+//   }
 // after adding currency parameter it works
+
+export function roundPrice(price: number, pattern: string = '%PRICE% %CURRENCY%', currency: string = 'SEK'): string {
+    const roundedPrice = Math.ceil(price * 100) / 100; 
+    const formattedPrice = roundedPrice.toFixed(2);
+    const formattedPattern = pattern.replace('%PRICE%', formattedPrice).replace('%CURRENCY%', currency);
+    return formattedPattern;
+  }
